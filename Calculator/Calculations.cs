9fc600a17ef1c32cf87850b44ea474fceb33c670
Alexis Borovik <borovikalexey@mail.ru>
2016-09-20 15:21:00 +0300

    Add testing project

--- Calculator/Calculations.cs
+++ Calculator/Calculations.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Calculations
+    public class Calculations
     {
         public enum Operations
         {
---
+++ CalculatorTesting/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Calculator;
+
+namespace CalculatorTesting
+{
+    public class Class1
+    {
+        Calculations calculations;
+        [SetUp]
+        public void Init()
+        {
+            calculations = new Calculations();
+        }
+        [Test]
+        public void TestSumTwoPositives()
+        {
+            Assert.AreEqual(4, calculations.Calculate(2, 2, Calculations.Operations.Add));
+        }
+        [Test]
+        public void TestSumTwoNegatives()
+        {
+            Assert.AreEqual(-4, calculations.Calculate(-2, -2, Calculations.Operations.Add));
+        }
+        [Test]
+        public void TestSumTwoDifferentSigns()
+        {
+            Assert.AreEqual(0, calculations.Calculate(-2, 2, Calculations.Operations.Add));
+        }
+        [Test]
+        public void TestSubTwoPositives()
+        {
+            Assert.AreEqual(0, calculations.Calculate(2, 2, Calculations.Operations.Subtract));
+        }
+        [Test]
+        public void TestSubTwoNegatives()
+        {
+            Assert.AreEqual(0, calculations.Calculate(-2, -2, Calculations.Operations.Subtract));
+        }
+        [Test]
+        public void TestSubTwoDifferentSigns()
+        {
+            Assert.AreEqual(-4, calculations.Calculate(-2, 2, Calculations.Operations.Subtract));
+        }
+        [Test]
+        public void TestMulTwoPositives()
+        {
+            Assert.AreEqual(4, calculations.Calculate(2, 2, Calculations.Operations.Multiply));
+        }
+        [Test]
+        public void TestMulTwoNegatives()
+        {
+            Assert.AreEqual(4, calculations.Calculate(-2, -2, Calculations.Operations.Multiply));
+        }
+        [Test]
+        public void TestMulTwoDifferentSigns()
+        {
+            Assert.AreEqual(-4, calculations.Calculate(-2, 2, Calculations.Operations.Multiply));
+        }
+        [Test]
+        public void TestDivTwoPositives()
+        {
+            Assert.AreEqual(2, calculations.Calculate(4, 2, Calculations.Operations.Divide));
+        }
+        [Test]
+        public void TestDivTwoNegatives()
+        {
+            Assert.AreEqual(2, calculations.Calculate(-4, -2, Calculations.Operations.Divide));
+        }
+        [Test]
+        public void TestDivTwoDifferentSigns()
+        {
+            Assert.AreEqual(-2, calculations.Calculate(-4, 2, Calculations.Operations.Divide));
+        }
+    }
+}
